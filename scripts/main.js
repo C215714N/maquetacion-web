a7351609534cabfac55ba8165c20d3ef655bab2c
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-06-30 03:42:30 -0300

    Logica Menu Hamburguesa

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/*Barra de Navegacion*/	
+	menuBtn = document.querySelector('nav .btn')
+	menuLst = document.querySelector('nav .menu')
+	
+	menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
