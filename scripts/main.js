0db7a2f79b8057e501924b089b8665760325f4ee
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-12 02:34:16 -0300

    Logica Factorizacion Busqueda

--- scripts/main.js
+++ scripts/main.js
@@ -12,22 +12,25 @@
 	prev.onclick = () => changeItem(slide, false)
 	next.onclick = () => changeItem(slide, true)
 
-	const changeItem = (itemList, type) => {
-		for(i = 0; i < itemList.length; i++){
-			if( itemList[i].classList.contains('active') ){
-				itemList[i].classList.remove('active')
-				switch(type){
-					case true:
-						i < itemList.length - 1 ? newItem = itemList[i + 1] : newItem = itemList[0]
-					break;
-					default:
-						i > 0 ?	newItem = itemList[i - 1] : newItem = itemList[itemList.length -1]
-		}	}	} 	newItem.classList.add('active')	
+	const changeItem = (array, type, className = 'active') => {
+		for(i = 0; i < array.length; i++){
+			if(array[i].classList.contains(className)){
+				array[i].classList.remove(className)
+				getDirection(array, type, i)
+		}	} 	newItem.classList.add(className)	
 	}
+
+	const getDirection = (array, type, i) => { 
+		switch(type){
+			case true: newItem = ( i < array.length - 1 ? array[i + 1] : array[0] )
+		break;
+			default: newItem = ( i > 0 ? array[i - 1] : array[array.length -1] )
+	}	}
 /*Redes Sociales*/
+	socialBtn = document.querySelector('#footer .social span')
 	socialBar = document.querySelector('#footer .social')
 	
-	socialBar.onclick = () => socialBar.classList.toggle('active')
+	socialBtn.onclick = () => socialBar.classList.toggle('active')
 /*Mapa de Contacto*/
 	map = document.querySelector('#footer iframe')
 	sizeBtn = document.querySelector('#footer .btn.opacity')
@@ -39,9 +42,34 @@
 	btn = document.querySelector('.bg-content .btn')
 
 	bgVid.onclick = () => vid.paused ? ( vid.play(), btn.innerHTML = 'PAUSE' ) : ( vid.pause(), btn.innerHTML = 'PLAY')
-
 /*Mostrar Ejemplos*/
 	titles = document.querySelectorAll('.example h4')
 	example = document.querySelectorAll('.flex, .grid')
 
-	titles.forEach((t,i) => t.onclick = () => example[i].classList.toggle('active') )
+	titles.forEach((t,i) => t.onclick = () => example[i].classList.toggle('active'))
+/*Buscador de Contenido*/
+	sideTxt = document.querySelector('#sideText')
+	sideBar = document.querySelector('#sideList')
+	sideLst = document.querySelectorAll('.sidebar :is(strong, em, li):not(.excluded)')
+	
+	sideTxt.oninput = (e) => {
+		term = e.target.value.toLowerCase()
+		searchFilter(sideLst, term, 'filter')
+		searchList('.filter')
+	}
+	const searchFilter = (array, data, className = 'filter') => array.forEach( el => el.innerText.toLowerCase().includes(data) ? el.classList.add(className) : el.classList.remove(className))
+	
+	const searchList = (className) => {
+		sideBar.innerHTML = ''
+		results = document.querySelectorAll(className)
+		results.forEach(r => sideBar.innerHTML += `<option value=${r.innerText}>${r.innerText}<option>`)
+	}
+/*Formulario de Contacto*/
+	contact = document.querySelector('#contact')
+	fields  = contact.querySelectorAll('input', 'textarea')
+
+	contact.onsubmit = (e) => fields.forEach(f => console.log(`${f.name}: ${f.value}`))
+/*Prevenciones de Comportamientos*/
+	forms = document.querySelectorAll('form')
+
+	forms.forEach(f => f.addEventListener('submit', (e) => e.preventDefault()))
